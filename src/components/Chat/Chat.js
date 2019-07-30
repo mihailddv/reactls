594b98c857d2c4ae8a3d2a30bd85c8319e0efc0e
Дедов Михаил Александрович <Dedov.Mikhail@zolotoy.ru>
2019-07-30 15:14:46 +0300

    add chat div

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Chat extends Component {
+  render() {
+    return (
+      <div className="chat">
+        <input type="text" className="input-message"/>
+      </div>
+    );
+  }
+}
+
+export default Chat;
