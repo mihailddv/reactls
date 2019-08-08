725ee081ba237d7f0cb170a2df64329999cb7a55
Дедов Михаил Александрович <Dedov.Mikhail@zolotoy.ru>
2019-08-08 15:45:01 +0300

    add home

--- src/components/AppRouter/AppRouter.js
+++ src/components/AppRouter/AppRouter.js
@@ -11,15 +11,49 @@
 // используйте стили из AppRouter.module.css
 
 import React, { Component } from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import styles from './AppRouter.module.css';
 import Home from '../Home';
 import InboxList from '../InboxList';
 import OutboxList from '../OutboxList';
 import InboxMail from '../InboxMail';
 import OutboxMail from '../OutboxMail';
+import classNames from 'classnames';
 
-class AppRouter extends  Component {
+class AppRouter extends Component {
+  render() {
 
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <nav className={styles.nav}>
+            <ul className={classNames(styles.navList, 't-nav-list')}>
+              <li className={styles.navElement}>
+                <a className={classNames(styles.link, 't-link-home')} aria-current="page" href="/app">Home</a>
+              </li>
+              <li className={styles.navElement}>
+                <a className={classNames(styles.link, 't-link-inbox')} href="/app/inbox">Inbox</a>
+              </li>
+              <li className={styles.navElement}>
+                <a className={classNames(styles.link, 't-link-outbox')} href="/app/outbox">Outbox</a>
+              </li>
+            </ul>
+          </nav>
+          <div className={styles.content}>            
+            <h3 className={styles.title}>Home</h3>
+            <Switch>/
+              <Route path="/app" exact component={Home} />
+              <Route path="/app/inbox" exact component={InboxList} />
+              <Route path="/app/outbox" exact component={OutboxList} />
+              <Route path="/app/inbox/:id" component={InboxMail} />
+              <Route path="/app/outbox/:id" component={OutboxMail} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    )
+
+  }
 }
 
 export default AppRouter;
