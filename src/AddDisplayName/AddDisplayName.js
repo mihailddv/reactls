5be54381fe591ea2138409db8cc580c2d9a28363
Дедов Михаил Александрович <Dedov.Mikhail@zolotoy.ru>
2019-08-09 11:47:22 +0300

    test

--- src/AddDisplayName/AddDisplayName.js
+++ src/AddDisplayName/AddDisplayName.js
@@ -4,7 +4,16 @@ import React, { Component } from 'react';
   Напишите простой HOC и укажите для него displayName
 */
 
-export const withDisplayName = (displayName) => {}
+export const withDisplayName = (displayName) => {
+
+  class addLoggedInUser extends displayName {
+    render() {
+      
+    }
+  }
+  
+  return addLoggedInUser
+}
 
 
 // export const withDisplayName = () => {}
