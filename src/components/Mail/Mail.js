eff116d9a73353ec01b71da38ee79f50b8c734e9
Дедов Михаил Александрович <Dedov.Mikhail@zolotoy.ru>
2019-08-12 10:15:08 +0300

    add mail.js

--- src/components/Mail/Mail.js
+++ src/components/Mail/Mail.js
@@ -1,2 +1,32 @@
 // Изучите файл `/cypress/integration/homework.spec.js`, чтобы понять,
 // какие классы должен использовать компонент.
+
+import React, { Component } from 'react';
+import styles from './Mail.module.css';
+
+class Mail extends Component {
+
+  render() {
+
+    const { to, from, body } = this.props;
+    const typemail = from ? 'from' : 'to';
+
+    return (
+
+      <div className={styles.container}>
+        <p className="t-mail-to">
+          { typemail === 'to' ? 'To: ' : 'From: '} 
+          <b>{ to || from }</b>
+        </p>
+        <p className="t-mail-body">
+          { body }
+        </p>
+      </div>
+
+    )
+
+  }
+
+}
+
+export default Mail;
