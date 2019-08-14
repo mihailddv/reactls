bd7f955f7b318c989c1c6be6d84fc9e897aa74c4
Дедов Михаил Александрович <Dedov.Mikhail@zolotoy.ru>
2019-08-14 15:36:17 +0300

    add create, movenext

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -7,8 +7,51 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 // Обратите внимание на `orders.test.js`.
 // Он поможет понять, какие значения должен возвращать редьюсер.
 
+const currentPosition = ({ position }) => {
+  return +position.substring(position.length - 1);
+};
+
+const nextPosition = ( { position, ingredients, recipe } ) => {
+
+  if ( position === 'clients' ) {
+    return 'conveyor_1';
+  } else if ( position !== 'conveyor_4' ) {
+    return `conveyor_${currentPosition({ position }) + 1}`;
+  } else if ( position === 'conveyor_4' && ingredients.length === recipe.length ) {
+    return 'finish';
+  } else {
+    return position;
+  }
+
+};
+
 export default (state = [], action) => {
   switch (action.type) {
+
+    case CREATE_NEW_ORDER:
+      return [
+        ...state,
+        {
+          id: action.payload.id,
+          ingredients: [],
+          position: 'clients',
+          recipe: action.payload.recipe
+        }
+      ];
+
+    case MOVE_ORDER_NEXT:
+      return state.map(item => {
+        if ( item.id === action.payload ) {
+          return {
+            ...item,
+            position: nextPosition(item)
+          }
+        } else {
+          return item
+        }
+      });
+
+
     default:
       return state;
   }
