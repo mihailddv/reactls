ef79da51e177c49534acc589f30c5ad807ff6393
Дедов Михаил Александрович <Dedov.Mikhail@zolotoy.ru>
2019-08-15 16:06:31 +0300

    add approuter

--- src/components/AppRouter/AppRouter.js
+++ src/components/AppRouter/AppRouter.js
@@ -1,3 +1,19 @@
 // Реализуйте роутер
 // Вам нужно определить корневой роут, который будет вести на страницу поиска.
 // Роут шоу должен принимать id в параметрах.
+
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import ShowPage from '../ShowPage';
+import Search from '../Search';
+
+const AppRouter = () => (
+  <div className="App">
+    <Switch>
+      <Route path="/" exact component={Search} />
+      <Route path="/shows/:id" exact component={ShowPage} />
+    </Switch>
+  </div>
+);
+
+export default AppRouter;
